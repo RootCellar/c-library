f10c2a6f7d7c605925c27f2b0a50d3a0eab80b50
RootCellar <RootCellar9877@gmail.com>
2024-05-04 13:57:28 -0800

    write some float and int benchmarks

--- benchmark.c
+++ benchmark.c
@@ -4,8 +4,11 @@
 
 #include "debug.h"
 #include "benchmark.h"
+#include "memory.h"
 
 #define BENCHMARK_TIMES (2)
+
+// Real world workload type functions
 
 int hit(float baseCrit, float* modifier) {
   float critChance = baseCrit + (*modifier);
@@ -21,15 +24,55 @@ int hit(float baseCrit, float* modifier) {
   return 0;
 }
 
+void malloc_and_free() {
+  void* ptr = malloc(sizeof(char) * 1 << 8);
+  free(ptr);
+}
+
+// float benchmarks
+
+float float_add(float f) {
+  return f + 5.5;
+}
+
+float float_subtract(float f) {
+  return f - 5.5;
+}
+
+float float_multiply(float f) {
+  return f * 5.5;
+}
+
+float float_divide(float f) {
+  return f / 5.5;
+}
+
+// int benchmarks
+
+int int_add(int i) {
+  return i + 7;
+}
+
+int int_subtract(int i) {
+  return i - 7;
+}
+
+int int_multiply(int i) {
+  return i * 7;
+}
+
+int int_divide(int i) {
+  return i / 7;
+}
+
+int int_mod(int i) {
+  return i % 7;
+}
+
 int main() {
   srand(time(NULL));
 
   benchmark_setupLocale();
-
-  // Hit
-
-  float modifier = 0.0;
-  TIMES( BENCHMARK_LOOPS( hit(0.05,&modifier), "hit", 1000000 ), BENCHMARK_TIMES);
 
   // rand
 
@@ -38,5 +81,33 @@ int main() {
   // clock
 
   TIMES( BENCHMARK_LOOPS( clock(), "clock", 1000000 ), BENCHMARK_TIMES);
+
+  // float benchmarks
+
+  int float_times = 100000000;
+
+  TIMES( BENCHMARK_LOOPS( float_add(9.0), "float_add", float_times ), BENCHMARK_TIMES);
+  TIMES( BENCHMARK_LOOPS( float_subtract(9.0), "float_subtract", float_times ), BENCHMARK_TIMES);
+  TIMES( BENCHMARK_LOOPS( float_multiply(9.0), "float_multiply", float_times ), BENCHMARK_TIMES);
+  TIMES( BENCHMARK_LOOPS( float_divide(9.0), "float_divide", float_times ), BENCHMARK_TIMES);
+
+  // int benchmarks
+
+  int int_times = 100000000;
+
+  TIMES( BENCHMARK_LOOPS( int_add(9), "int_add", int_times ), BENCHMARK_TIMES);
+  TIMES( BENCHMARK_LOOPS( int_subtract(9), "int_subtract", int_times ), BENCHMARK_TIMES);
+  TIMES( BENCHMARK_LOOPS( int_multiply(9), "int_multiply", int_times ), BENCHMARK_TIMES);
+  TIMES( BENCHMARK_LOOPS( int_divide(9), "int_divide", int_times ), BENCHMARK_TIMES);
+  TIMES( BENCHMARK_LOOPS( int_mod(9), "int_mod", int_times ), BENCHMARK_TIMES);
+
+  // Hit
+
+  float modifier = 0.0;
+  TIMES( BENCHMARK_LOOPS( hit(0.05,&modifier), "hit", 1000000 ), BENCHMARK_TIMES);
+
+  // malloc_and_free
+
+  TIMES( BENCHMARK_LOOPS( malloc_and_free(), "malloc_and_free", 1000000 ), BENCHMARK_TIMES);
   
 }
