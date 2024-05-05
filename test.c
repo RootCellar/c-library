f34b6ee378187b06830d59f7e8d436703523e5ce
RootCellar <RootCellar9877@gmail.com>
2024-05-04 16:57:24 -0800

    test memory.h a little more

--- test.c
+++ test.c
@@ -2,16 +2,24 @@
 #include "memory.h"
 
 void main() {
+
+  // Basic checks
+
   TEST( tResize(-1) == 1, "tResize(-1)");
   TEST( tResize(0) == 1, "tResize(0)");
 
   TEST( tResize(1) == 0, "tResize(1)");
   TEST( tResize(1) == 0, "tResize(1) again");
 
+  TEST( tFree(0) == 0, "free a null pointer");
+  TEST( tFree( (void*) 16 ) == 1, "free a pointer that isn't in the list");
+
   TEST( tMalloc(0) == NULL, "tMalloc(0)");
 
   TEST( tGetTotalAllocs() == 0, "tGetTotalAllocs() with nothing allocated");
   TEST( tGetTotalAllocSize() == 0, "tGetTotalAllocSize() with nothing allocated");
+
+  // Using the memory allocator
 
   size_t alloc_size = 10;
   size_t alloc_bytes = alloc_size * sizeof(int);
@@ -32,5 +40,16 @@ void main() {
 
   TEST( tGetTotalAllocs() == 0, "tGetTotalAllocs() after tFree()");
   TEST( tGetTotalAllocSize() == 0, "tGetTotalAllocSize() after tFree()");
+
+  ptr = tMalloc(alloc_bytes);
+  TEST( tMalloc(alloc_bytes) == NULL, "pointer list too small");
+  TEST( tFree(ptr) == 0, "tFree() the pointer");
+
+  TEST( tResize(10) == 0, "expand pointer list");
+  TEST( tResize(1) == 0, "shrink pointer list");
+
+  ptr = tMalloc(alloc_bytes);
+  TEST( tMalloc(alloc_bytes) == NULL, "tMalloc() pointer list too small");
+  TEST( tFree(ptr) == 0, "tFree() the pointer");
   
 }
