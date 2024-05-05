f894d3771773a16995900c45262cc00a361cfb75
RootCellar <RootCellar9877@gmail.com>
2024-05-05 00:43:17 -0800

    print benchmark lines in cyan

--- benchmark.h
+++ benchmark.h
@@ -3,16 +3,17 @@
 
 #include <locale.h>
 
+#include "terminal_color.h"
 
 #define BENCHMARK_TIME(x, name, time) do { int runCount = 0; clock_t start_time = clock(); clock_t time_now;\
     while( ( time_now = clock() ) - start_time < CLOCKS_PER_SEC * time) { x; ++runCount; }\
     float seconds = (float) (time_now - start_time) / CLOCKS_PER_SEC; \
-    printf(name ": %'d over %'f seconds \n", runCount, seconds, ((float)runCount/seconds)); } while(0)
+    printf(ANSI_COLOR_CYAN name ": %'d over %'f seconds " ANSI_COLOR_RESET "\n", runCount, seconds, ((float)runCount/seconds)); } while(0)
 
 #define BENCHMARK_LOOPS(x, name, loops) do { clock_t start_time = clock();\
     for( int i = 0; i < loops; i++ ) { x; }\
     float seconds = (float) (clock() - start_time) / CLOCKS_PER_SEC; \
-    printf(name ": %'d over %'f seconds ( %'f per second ) \n", loops, seconds, ((float)loops/seconds)); } while(0)
+    printf(ANSI_COLOR_CYAN name ": %'d over %'f seconds ( %'f per second ) " ANSI_COLOR_RESET "\n", loops, seconds, ((float)loops/seconds)); } while(0)
 
 
 // Do whatever x is the given number of times
