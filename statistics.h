2f4548e6fecc483d54de482d3b18cd3418ed71cc
RootCellar <RootCellar9877@gmail.com>
2024-05-05 00:28:30 -0800

    implement statistics_average_deviation() and statistics_standard_deviation() and test

--- statistics.h
+++ statistics.h
@@ -1,7 +1,14 @@
 #ifndef STATISTICS_H
 #define STATISTICS_H
 
+#include <math.h>
+
 #include "debug.h"
+
+float statistics_float_abs(float value) {
+  if(value < 0.0) return value * -1.0;
+  else return value;
+}
 
 float statistics_average(float* values, size_t count) {
 
@@ -11,6 +18,29 @@ float statistics_average(float* values, size_t count) {
   }
 
   return sum / (float)count;
+}
+
+float statistics_average_deviation(float* values, size_t count) {
+
+  float average = statistics_average(values, count);
+
+  float deviation_sum = 0;
+  for(size_t i = 0; i < count; i++) {
+    deviation_sum += statistics_float_abs( values[i] - average );
+  }
+
+  return deviation_sum / (float)count;
+}
+
+float statistics_standard_deviation(float* values, size_t count) {
+  float average = statistics_average(values, count);
+
+  float deviation_sum = 0;
+  for(size_t i = 0; i < count; i++) {
+    deviation_sum += pow(statistics_float_abs( values[i] - average ), 2.0);
+  }
+
+  return (float) sqrt((double) deviation_sum / (double)count);
 }
 
 #endif
--- test_statistics.c
+++ test_statistics.c
@@ -8,8 +8,12 @@ void main() {
 
   float two_values[] = {0.0, 10.0};
   TEST( floats_equal( statistics_average(two_values, 2), 5.0 ), "average of two values" );
+  TEST( floats_equal( statistics_average_deviation(two_values, 2), 5.0 ), "average deviation of two values" );
+  TEST( floats_equal( statistics_standard_deviation(two_values, 2), 5.0 ), "standard deviation of two values" );
 
   float many_values[] = {0.0, 10.0, 5.0, 2.5, 7.5, -5.0, 15.0};
   TEST( floats_equal( statistics_average(many_values, 7), 5.0 ), "average of many values" );
+  TEST( floats_equal( statistics_average_deviation(many_values, 7), 5.0 ), "statistics_average_deviation of many values" );
+  TEST( floats_equal( statistics_standard_deviation(many_values, 7), 6.1237243569579 ), "statistics_standard_deviation of many values" );
   
 }
