e6e5ac6a624cb8eecc00c7c1abf27591b83a5bcd
RootCellar <RootCellar9877@gmail.com>
2024-05-04 22:30:28 -0800

    adjust spacing

--- test.h
+++ test.h
@@ -27,8 +27,8 @@
         do { fprintf(stderr, "%s: %s() line %d \n\n", __FILE__, \
                                  __func__, __LINE__); } while (0)
 
-#define PASS_TEST(expr, name) do { printf(" "   ANSI_COLOR_GREEN   "Test: " name " -- " #expr " -- PASS   " ANSI_COLOR_RESET "\n"); } while(0)
-#define FAIL_TEST(expr, name) do { printf("\n " ANSI_COLOR_RED     "Test: " name " -- " #expr " -- FAILED "  ANSI_COLOR_RESET  "\n"); printf("** TEST FAILED ** \n"); } while(0)
+#define PASS_TEST(expr, name) do { printf(""   ANSI_COLOR_GREEN   "Test: " name " -- " #expr " -- PASS   "  ANSI_COLOR_RESET "\n"); } while(0)
+#define FAIL_TEST(expr, name) do { printf("\n" ANSI_COLOR_RED     "Test: " name " -- " #expr " -- FAILED "  ANSI_COLOR_RESET "\n"); printf("** TEST FAILED ** \n"); } while(0)
 
 // Test that the given expression is true
 #define TEST(expr, name) \
@@ -39,7 +39,7 @@ else { FAIL_TEST(expr, name); SHOW_LOCATION(); if(FAIL_FAST) exit(EXIT_FAILURE);
 
 #define SECTION(name) \
 do { \
-printf("\n\n " ANSI_COLOR_BRIGHT_YELLOW "** " name " ** " ANSI_COLOR_RESET "\n\n\n");\
+printf("\n\n  " ANSI_COLOR_BRIGHT_YELLOW "** " name " ** " ANSI_COLOR_RESET "\n\n\n");\
 } while(0)
 
 #endif
