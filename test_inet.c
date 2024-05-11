f736e1f373cf1e4d27535e8ecdfe1d31834011cc
RootCellar <RootCellar9877@gmail.com>
2024-05-10 20:56:20 -0800

    work on testing internet header

--- test_inet.c
+++ test_inet.c
@@ -1,16 +1,36 @@
-#include "test.h"
+#include <time.h>
 
+#include "test.h"
 #include "inet.h"
 
 void main() {
-  TEST( create_connection("localhost", 40000) < 0, "server can't be connected to" );
+  int result = 0;
+
+  int client_socket = create_connection("localhost", 40000);
+  time_t start = clock();
+  time_t now = start;
+  while(now - start < CLOCKS_PER_SEC && result >= 0) {
+    result = is_connected(client_socket);
+    now = clock();
+  }
+
+  debug_printf("connect result: %d", result);
+  TEST( result < 0, "server can't be connected to" );
 
   int server_socket = create_server_socket(40000);
   TEST( server_socket >= 0, "server socket created and bound" );
 
-  int client_socket = create_connection("localhost", 40000);
+  TEST( accept_connection(server_socket) == -1, "accept_connection while no clients are connecting");
+
+  client_socket = create_connection("localhost", 40000);
   TEST( client_socket >= 0, "server can be connected to" );
+
+  int server_fd_to_client;
+  server_fd_to_client = accept_connection(server_socket);
+  TEST( accept_connection(server_socket) >= 0, "accept_connection accepted a connection");
 
   char* to_write = "hello from the client!";
   TEST( send_string(client_socket, to_write) == 0, "client successfully sends a string to the server");
+
+  
 }
