1f6f9999b12a218372dc0868c33f0f93e1366351
RootCellar <RootCellar9877@gmail.com>
2024-05-29 07:48:33 -0800

    implement additional vector3 functions

--- vector.h
+++ vector.h
@@ -26,6 +26,16 @@ struct Vector3 vector3_normalize(struct Vector3 vector) {
   return toRet;
 }
 
+struct Vector3 vector3_negate(struct Vector3 vector) {
+  struct Vector3 toRet;
+
+  toRet.x = vector.x * -1;
+  toRet.y = vector.y * -1;
+  toRet.z = vector.z * -1;
+
+  return toRet;
+}
+
 struct Vector3 vector3_add(struct Vector3 one, struct Vector3 two) {
   struct Vector3 toRet;
 
@@ -34,6 +44,10 @@ struct Vector3 vector3_add(struct Vector3 one, struct Vector3 two) {
   toRet.z = one.z + two.z;
 
   return toRet;
+}
+
+struct Vector3 vector3_subtract(struct Vector3 one, struct Vector3 two) {
+  return vector3_add( one, vector3_negate(two) );
 }
 
 struct Vector3 vector3_multiply_scalar(struct Vector3 vector, float mult) {
@@ -49,6 +63,20 @@ struct Vector3 vector3_multiply_scalar(struct Vector3 vector, float mult) {
 struct Vector3 vector3_to_length(struct Vector3 vector, float len) {
   struct Vector3 normalized = vector3_normalize(vector);
   return vector3_multiply_scalar(normalized, len);
+}
+
+float vector3_dot_product(struct Vector3 one, struct Vector3 two) {
+  return one.x * two.x + one.y * two.y + one.z * two.z;
+}
+
+struct Vector3 vector3_cross_product(struct Vector3 one, struct Vector3 two) {
+  struct Vector3 toRet;
+
+  toRet.x = one.y * two.z - one.z * two.y;
+  toRet.y = one.z * two.x - one.x * two.z;
+  toRet.z = one.x * two.y - one.y * two.x;
+
+  return toRet;
 }
 
 #endif //VECTOR_H
