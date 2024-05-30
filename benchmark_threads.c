907b55ae61613981360024732d90a59bd24c407a
RootCellar <RootCellar9877@gmail.com>
2024-05-29 20:52:34 -0800

    show actual trig ops per second

--- benchmark_threads.c
+++ benchmark_threads.c
@@ -6,7 +6,8 @@
 #include "sthread.h"
 #include "benchmark.h"
 
-#define NUM_LOOPS_SIMPLE 100000
+#define NUM_LOOPS_SIMPLE 500000
+#define BENCHMARK_LOOPS 100
 #define THREAD_COUNT 16
 
 void modify_values(int id, int count, void* data) {
@@ -21,13 +22,22 @@ void main() {
 
   srand(time(NULL));
 
+  benchmark_setupLocale();
+
   int data[THREAD_COUNT];
 
   for(int i = 0; i < THREAD_COUNT; i++) {
     data[i] = 0;
   }
 
-  TIMES( BENCHMARK_LOOPS( run_in_threads(modify_values, (void*) data, THREAD_COUNT, THREAD_COUNT), "", 100), 4 );
+
+  float speed;
+
+  for(int i = 0; i < 5; i++) {
+    BENCHMARK_LOOPS_CODE( run_in_threads(modify_values, (void*) data, THREAD_COUNT, THREAD_COUNT), speed, BENCHMARK_LOOPS);
+
+    printf("Total trig ops per second: %'f\n", speed * (float) BENCHMARK_LOOPS * (float) NUM_LOOPS_SIMPLE * (float) THREAD_COUNT);
+  }
 
   exit(0);
 
