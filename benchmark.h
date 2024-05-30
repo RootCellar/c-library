c27c0c488852386b59f9732e7dadf9264c3ac246
RootCellar <RootCellar9877@gmail.com>
2024-05-29 20:33:08 -0800

    rework benchmark.h to deliver more accurate results

--- benchmark.h
+++ benchmark.h
@@ -3,24 +3,25 @@
 
 #include <locale.h>
 #include <time.h>
+#include <sys/time.h>
+#include <stdlib.h>
 
 #include "terminal_color.h"
 
 #define SHOW_ALL 0
 
-#define BENCHMARK_TIME(x, name, time) do { int runCount = 0; clock_t start_time = clock(); clock_t time_now;\
-    while( ( time_now = clock() ) - start_time < CLOCKS_PER_SEC * time) { x; ++runCount; }\
-    float seconds = (float) (time_now - start_time) / CLOCKS_PER_SEC; \
-    printf(ANSI_COLOR_BRIGHT_CYAN name ": %'d over %'f seconds " ANSI_COLOR_RESET "\n", runCount, seconds, ((float)runCount/seconds)); } while(0)
-
-#define BENCHMARK_LOOPS(x, name, loops) do { clock_t start_time = clock();\
+#define BENCHMARK_LOOPS(x, name, loops) do { struct timespec start_time = get_time();\
     for( int i = 0; i < loops; i++ ) { x; }\
-    float seconds = (float) (clock() - start_time) / CLOCKS_PER_SEC; \
+    struct timespec end = get_time();\
+    start_time.tv_sec -= end.tv_sec; end.tv_sec=0; \
+    double seconds = timespec_to_double(end) - timespec_to_double(start_time); \
     printf(ANSI_COLOR_BRIGHT_CYAN name ": %'d over %'f seconds ( %'f per second ) " ANSI_COLOR_RESET "\n", loops, seconds, ((float)loops/seconds)); } while(0)
 
-#define BENCHMARK_LOOPS_CODE(x, var, loops) do { clock_t start_time = clock();\
+#define BENCHMARK_LOOPS_CODE(x, var, loops) do { struct timespec start_time = get_time();\
     for( int i = 0; i < loops; i++ ) { x; }\
-    float seconds = (float) (clock() - start_time) / CLOCKS_PER_SEC; float runs_per_second = ((float)loops/seconds); \
+    struct timespec end = get_time();\
+    start_time.tv_sec -= end.tv_sec; end.tv_sec=0; \
+    double seconds = timespec_to_double(end) - timespec_to_double(start_time); float runs_per_second = ((float)loops/seconds); \
     if(SHOW_ALL) printf(ANSI_COLOR_BRIGHT_CYAN #x ": %'d over %'f seconds ( %'f per second ) " ANSI_COLOR_RESET "\n", loops, seconds, runs_per_second); var = runs_per_second; } while(0)
 
 // Do whatever x is the given number of times
@@ -30,6 +31,21 @@ void benchmark_setupLocale() {
     // set the locale so that printed numbers can be formatted
     // with commas separating the digits
     setlocale(LC_NUMERIC, "");
+}
+
+double timespec_to_double(struct timespec time) {
+    double x2 = ((float)time.tv_nsec) / 1000000000.0f;
+    double x1 = (float) time.tv_sec;
+    double x = x1 + x2;
+    // printf("%ld %ld\n", time.tv_sec, time.tv_nsec);
+    // printf("%lf %lf %lf\n", x, x1, x2);
+    return x;
+}
+
+struct timespec get_time() {
+    struct timespec time;
+    clock_gettime(CLOCK_REALTIME, &time);
+    return time;
 }
 
 #endif
