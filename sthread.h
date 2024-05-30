b0f808aca5db564cf864822021857588ef1dc426
RootCellar <RootCellar9877@gmail.com>
2024-05-29 16:08:32 -0800

    implement sthread.h

---
+++ sthread.h
@@ -0,0 +1,56 @@
+
+#ifndef STHREAD_H
+#define STHREAD_H
+
+#include <pthread.h>
+
+#include "debug.h"
+
+struct thread_args {
+  int id;
+  int thread_count;
+  void* items;
+  int count;
+  void (*function)(int,int,void*);
+};
+
+void dump_thread_args(struct thread_args* args) {
+  debug_printf("%d %d %p %d %p", args->id, args->thread_count, args->items, args->count, args->function);
+}
+
+void start_thread(void* args) {
+  struct thread_args* thread_data = (struct thread_args*) (args);
+
+  dump_thread_args(thread_data);
+
+  (thread_data->function)(thread_data->id, thread_data->thread_count, thread_data->items);
+
+  pthread_exit(0);
+}
+
+void run_in_threads(void* function, void* items, int count, int thread_count) {
+
+  pthread_t threads[thread_count];
+  struct thread_args args[thread_count];
+
+  for(int i = 0; i < thread_count; i++) {
+    args[i].id = i;
+    args[i].thread_count = thread_count;
+    args[i].items = items;
+    args[i].count = count;
+    args[i].function = function;
+
+    debug_printf("%d", i);
+
+    pthread_create(&threads[i], NULL, start_thread, (void*)&(args[i]));
+  }
+
+  for(int i = 0; i < thread_count; i++)
+  {
+    debug_printf("Waiting on %d...", i);
+    pthread_join(threads[i], NULL);
+  }
+
+}
+
+#endif //STHREAD_H
