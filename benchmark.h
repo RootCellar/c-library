fd00e27233a534d93cdbce4f11d15d3e4049358b
RootCellar <RootCellar9877@gmail.com>
2024-05-31 13:06:32 -0800

    correct time tracking

--- benchmark.h
+++ benchmark.h
@@ -13,14 +13,14 @@
 #define BENCHMARK_LOOPS(x, name, loops) do { struct timespec start_time = get_time();\
     for( int i = 0; i < loops; i++ ) { x; }\
     struct timespec end = get_time();\
-    start_time.tv_sec -= end.tv_sec; end.tv_sec=0; \
+    end.tv_sec -= start_time.tv_sec; start_time.tv_sec=0; \
     double seconds = timespec_to_double(end) - timespec_to_double(start_time); \
     printf(ANSI_COLOR_BRIGHT_CYAN name ": %'d over %'f seconds ( %'f per second ) " ANSI_COLOR_RESET "\n", loops, seconds, ((float)loops/seconds)); } while(0)
 
 #define BENCHMARK_LOOPS_CODE(x, var, loops) do { struct timespec start_time = get_time();\
     for( int i = 0; i < loops; i++ ) { x; }\
     struct timespec end = get_time();\
-    start_time.tv_sec -= end.tv_sec; end.tv_sec=0; \
+    end.tv_sec -= start_time.tv_sec; start_time.tv_sec=0; \
     double seconds = timespec_to_double(end) - timespec_to_double(start_time); float __runs_per_second = ((float)loops/seconds); \
     if(SHOW_ALL) printf(ANSI_COLOR_BRIGHT_CYAN #x ": %'d over %'f seconds ( %'f per second ) " ANSI_COLOR_RESET "\n", loops, seconds, __runs_per_second); var = __runs_per_second; } while(0)
 
