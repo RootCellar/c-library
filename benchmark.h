a550c1df4084bee9e93ec651a95d19a75082dd7c
RootCellar <RootCellar9877@gmail.com>
2024-05-31 13:11:11 -0800

    extract header time.h

--- benchmark.h
+++ benchmark.h
@@ -2,10 +2,8 @@
 #define BENCHMARK_H
 
 #include <locale.h>
-#include <time.h>
-#include <sys/time.h>
-#include <stdlib.h>
 
+#include "time.h"
 #include "terminal_color.h"
 
 #define SHOW_ALL 0
@@ -13,15 +11,13 @@
 #define BENCHMARK_LOOPS(x, name, loops) do { struct timespec start_time = get_time();\
     for( int i = 0; i < loops; i++ ) { x; }\
     struct timespec end = get_time();\
-    end.tv_sec -= start_time.tv_sec; start_time.tv_sec=0; \
-    double seconds = timespec_to_double(end) - timespec_to_double(start_time); \
+    double seconds = timespec_difference_seconds(start_time, end); \
     printf(ANSI_COLOR_BRIGHT_CYAN name ": %'d over %'f seconds ( %'f per second ) " ANSI_COLOR_RESET "\n", loops, seconds, ((float)loops/seconds)); } while(0)
 
 #define BENCHMARK_LOOPS_CODE(x, var, loops) do { struct timespec start_time = get_time();\
     for( int i = 0; i < loops; i++ ) { x; }\
     struct timespec end = get_time();\
-    end.tv_sec -= start_time.tv_sec; start_time.tv_sec=0; \
-    double seconds = timespec_to_double(end) - timespec_to_double(start_time); float __runs_per_second = ((float)loops/seconds); \
+    double seconds = timespec_difference_seconds(start_time, end); float __runs_per_second = ((float)loops/seconds); \
     if(SHOW_ALL) printf(ANSI_COLOR_BRIGHT_CYAN #x ": %'d over %'f seconds ( %'f per second ) " ANSI_COLOR_RESET "\n", loops, seconds, __runs_per_second); var = __runs_per_second; } while(0)
 
 // Do whatever x is the given number of times
@@ -31,21 +27,6 @@ void benchmark_setupLocale() {
     // set the locale so that printed numbers can be formatted
     // with commas separating the digits
     setlocale(LC_NUMERIC, "");
-}
-
-double timespec_to_double(struct timespec time) {
-    double x2 = (double) time.tv_nsec / 1000000000.0;
-    double x1 = time.tv_sec;
-    double x = x1 + x2;
-    // printf("%ld %ld\n", time.tv_sec, time.tv_nsec);
-    // printf("%lf %lf %lf\n", x, x1, x2);
-    return x;
-}
-
-struct timespec get_time() {
-    struct timespec time;
-    clock_gettime(CLOCK_REALTIME, &time);
-    return time;
 }
 
 #endif
---
+++ time.h
@@ -0,0 +1,34 @@
+
+#ifndef TIME_H
+#define TIME_H
+
+#include <time.h>
+#include <sys/time.h>
+
+double timespec_to_double(struct timespec time) {
+  double nanoseconds_in_seconds = (double) time.tv_nsec / 1000000000.0;
+  double seconds = time.tv_sec;
+  double timespec_in_seconds = seconds + nanoseconds_in_seconds;
+  // printf("%ld %ld\n", time.tv_sec, time.tv_nsec);
+  // printf("%lf %lf %lf\n", x, x1, x2);
+  return timespec_in_seconds;
+}
+
+double timespec_difference_seconds(struct timespec start, struct timespec end) {
+
+  // Shrink tv_sec down to the actual integer difference between them
+  // Helps protect time measurements from floating point roundoff because
+  // tv_sec is a large number
+  end.tv_sec -= start.tv_sec;
+  start.tv_sec = 0;
+
+  return timespec_to_double(end) - timespec_to_double(start);
+}
+
+struct timespec get_time() {
+  struct timespec time;
+  clock_gettime(CLOCK_REALTIME, &time);
+  return time;
+}
+
+#endif //TIME_H
