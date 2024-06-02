258ecd450e2ca7d50e88a4723b8ea935e4acb0e9
RootCellar <RootCellar9877@gmail.com>
2024-06-02 01:04:00 -0800

    rename NUM_LOOPS_SIMPLE to NUM_LOOPS_TRIG

--- benchmark_threads.c
+++ benchmark_threads.c
@@ -6,13 +6,13 @@
 #include "sthread.h"
 #include "benchmark.h"
 
-#define NUM_LOOPS_SIMPLE 500000
 #define BENCHMARK_LOOPS 100
+#define NUM_LOOPS_TRIG 500000
 #define THREAD_COUNT 16
 
 void modify_values(int id, int thread_count, int count, void* data) {
   float* actual_data = (float*) data;
-  for(int i = 0; i < NUM_LOOPS_SIMPLE; i++) {
+  for(int i = 0; i < NUM_LOOPS_TRIG; i++) {
     actual_data[id] += cosf(i);
     actual_data[id] += sinf(i);
   }
@@ -36,7 +36,7 @@ void main() {
   for(int i = 0; i < 5; i++) {
     BENCHMARK_LOOPS_CODE( run_in_threads(modify_values, (void*) data, THREAD_COUNT, THREAD_COUNT), runs_per_second, BENCHMARK_LOOPS);
 
-    printf("Total trig ops per second: %'f\n", runs_per_second * (float) NUM_LOOPS_SIMPLE * (float) THREAD_COUNT);
+    printf("Total trig ops per second: %'f\n", runs_per_second * (float) NUM_LOOPS_TRIG * (float) THREAD_COUNT);
   }
 
   exit(0);
