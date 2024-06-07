4c9d229412212494aac6e587026f047528de8926
RootCellar <RootCellar9877@gmail.com>
2024-06-06 20:01:21 -0800

    test neural evaluation

--- neural.c
+++ neural.c
@@ -5,6 +5,13 @@ void main() {
 
   struct NeuralNet neural_net = create_neural_net(8, 8);
 
+  int count = 1;
+  float input[count];
+
+  input[0] = 1;
+  float result = neural_evaluate(&neural_net, input, count);
+  printf("Result: %f\n", result);
+
   free_neural_net(&neural_net);
   tFreePointerList();
 }
