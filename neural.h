bb577d147ff2287061b36912ac67eebeb1cac14d
RootCellar <RootCellar9877@gmail.com>
2024-06-06 17:20:10 -0800

    lay out basis for neural.h

--- neural.h
+++ neural.h
@@ -20,5 +20,25 @@ struct Neuron {
   int input_count;
 };
 
+void free_neural_net(struct NeuralNet* neural_net) {
+}
+
+struct Neuron create_neuron(int input_count) {
+}
+
+struct NeuralNet create_neural_net(int layers, int neurons_per_layer) {
+}
+
+float activation_tanh(float output) {
+  return tanh(output);
+}
+
+float neural_evaluate(struct NeuralNet* net, float* input) {
+
+}
+
+float neural_train(struct NeuralNet* net, float* input, float* correct_output) {
+
+}
 
 #endif
