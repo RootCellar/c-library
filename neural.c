c72dd4cf77d1d3f4d8b0f1300a17db761e7e4a27
RootCellar <RootCellar9877@gmail.com>
2024-06-08 15:35:18 -0800

    test improved training

--- neural.c
+++ neural.c
@@ -7,10 +7,13 @@ void main() {
 
   tResize(8192);
 
-  struct NeuralNet neural_net = create_neural_net(2, 2);
+  struct NeuralNet neural_net = create_neural_net(3, 3);
 
   float onehalf = 0.5;
+  float onefifth = 1.0 / 5.0;
+  float onethird = 1.0 / 3.0;
   float one = 1.0;
+  float negone = -1.0;
 
   float result = neural_evaluate(&neural_net, &one, 1);
   printf("Result: %f\n", result);
@@ -20,14 +23,22 @@ void main() {
   settings.learning_rate = 0.1;
 
   for(int i = 0; i < 500000; i++) {
-    neural_train(&neural_net, settings, &one, 1, onehalf);
+    neural_train(&neural_net, settings, &one, 1, negone);
     neural_train(&neural_net, settings, &onehalf, 1, one);
+    neural_train(&neural_net, settings, &onethird, 1, negone);
+    neural_train(&neural_net, settings, &onefifth, 1, negone);
   }
 
   result = neural_evaluate(&neural_net, &one, 1);
   printf("Result: %f\n", result);
 
   result = neural_evaluate(&neural_net, &onehalf, 1);
+  printf("Result: %f\n", result);
+
+  result = neural_evaluate(&neural_net, &onethird, 1);
+  printf("Result: %f\n", result);
+
+  result = neural_evaluate(&neural_net, &onefifth, 1);
   printf("Result: %f\n", result);
 
   free_neural_net(&neural_net);
