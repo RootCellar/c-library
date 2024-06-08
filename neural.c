671fb70b310fbafc278923bab4f90cedc6123a28
RootCellar <RootCellar9877@gmail.com>
2024-06-08 12:59:03 -0800

    test neural net training

--- neural.c
+++ neural.c
@@ -1,15 +1,33 @@
 #include "neural.h"
 
+#include <time.h>
+
 void main() {
+  srand(time(NULL));
+
   tResize(8192);
 
-  struct NeuralNet neural_net = create_neural_net(8, 8);
+  struct NeuralNet neural_net = create_neural_net(2, 2);
 
-  int count = 1;
-  float input[count];
+  float onehalf = 0.5;
+  float one = 1.0;
 
-  input[0] = 1;
-  float result = neural_evaluate(&neural_net, input, count);
+  float result = neural_evaluate(&neural_net, &one, 1);
+  printf("Result: %f\n", result);
+
+  struct Net_Training_Settings settings;
+  settings.acceptable_error = 0.001;
+  settings.learning_rate = 0.1;
+
+  for(int i = 0; i < 5000000; i++) {
+    neural_train(&neural_net, settings, &one, 1, onehalf);
+    neural_train(&neural_net, settings, &onehalf, 1, one);
+  }
+
+  result = neural_evaluate(&neural_net, &one, 1);
+  printf("Result: %f\n", result);
+
+  result = neural_evaluate(&neural_net, &onehalf, 1);
   printf("Result: %f\n", result);
 
   free_neural_net(&neural_net);
