010e1ae95e6b9386493405cce41878eba88bf551
RootCellar <RootCellar9877@gmail.com>
2024-09-09 16:31:08 -0700

    allow names of tests to be generated, as well as hardcoded

--- test.h
+++ test.h
@@ -18,8 +18,8 @@
         do { fprintf(stderr, "%s: %s() line %d \n\n", __FILE__, \
                                  __func__, __LINE__); } while (0)
 
-#define PASS_TEST(expr, name) do { printf(""   TEST_PASS_OUTPUT_COLOR   " Test: " name " -- " #expr " -- PASS   "  ANSI_COLOR_RESET "\n"); } while(0)
-#define FAIL_TEST(expr, name) do { printf("\n" TEST_FAIL_OUTPUT_COLOR   " Test: " name " -- " #expr " -- FAILED "  ANSI_COLOR_RESET "\n"); printf(TEST_FAIL_OUTPUT_COLOR " ** TEST FAILED ** " ANSI_COLOR_RESET "\n"); SHOW_LOCATION(); if(FAIL_FAST) exit(EXIT_FAILURE); } while(0)
+#define PASS_TEST(expr, name) do { printf(""   TEST_PASS_OUTPUT_COLOR   " Test: %s -- " #expr " -- PASS   "  ANSI_COLOR_RESET "\n", name); } while(0)
+#define FAIL_TEST(expr, name) do { printf("\n" TEST_FAIL_OUTPUT_COLOR   " Test: %s -- " #expr " -- FAILED "  ANSI_COLOR_RESET "\n", name); printf(TEST_FAIL_OUTPUT_COLOR " ** TEST FAILED ** " ANSI_COLOR_RESET "\n"); SHOW_LOCATION(); if(FAIL_FAST) exit(EXIT_FAILURE); } while(0)
 
 // Test that the given expression is true
 #define __TEST(expr, name, quiet_pass) \
