5611f9ec97164c9ff9c3dbc0c5059aa06fa568ba
RootCellar <RootCellar9877@gmail.com>
2024-09-14 14:20:50 -0700

    improve dice program to show separate rolls in one run

--- dice.c
+++ dice.c
@@ -5,6 +5,7 @@
 
 void show_usage(void) {
   printf("Usage: dice [count] <sides>\n");
+  exit(1);
 }
 
 int main(int argc, char** argv) {
@@ -14,29 +15,39 @@ int main(int argc, char** argv) {
   int sides;
   int count = 1;
 
+  int separate = 0;
+
   if(argc < 2) {
     show_usage();
-    exit(1);
   } else if(argc == 2) {
     if(sscanf(argv[1], "%d", &sides) != 1) {
       show_usage();
-      exit(1);
     }
   } else if(argc == 3) {
-    if(sscanf(argv[1], "%d", &count) != 1 || sscanf(argv[2], "%d", &sides) != 1) {
+    if(sscanf(argv[1], "%d", &count) != 1) {
+      separate = 1;
+      if(sscanf(argv[1], "s%d", &count) != 1) {
+        show_usage();
+      }
+    }
+
+    if(sscanf(argv[2], "%d", &sides) != 1) {
       show_usage();
-      exit(1);
     }
   } else {
     show_usage();
-    exit(1);
   }
 
   if(count < 1 || sides < 1) {
     show_usage();
-    exit(1);
   }
 
-  printf("%d\n", roll_dice(count, sides));
+  if(!separate) printf("%d\n", roll_dice(count, sides));
+  else {
+    for(int i = 0; i < count; i++) {
+      printf("%d ", roll_die(sides));
+    }
+    printf("\n");
+  }
   exit(0);
 }
