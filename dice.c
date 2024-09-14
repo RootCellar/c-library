c07c9f02f00cf25e4dd42f7efba31fd4839b7ee9
RootCellar <RootCellar9877@gmail.com>
2024-09-14 13:38:23 -0700

    implement program to roll dice for user

---
+++ dice.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+#include "random.h"
+
+void show_usage(void) {
+  printf("Usage: dice [count] <sides>\n");
+}
+
+int main(int argc, char** argv) {
+
+  int sides;
+  int count = 1;
+
+  if(argc < 2) {
+    show_usage();
+    exit(1);
+  } else if(argc == 2) {
+    if(sscanf(argv[1], "%d", &sides) != 1) {
+      show_usage();
+      exit(1);
+    }
+  } else if(argc == 3) {
+    if(sscanf(argv[1], "%d", &count) != 1 || sscanf(argv[2], "%d", &sides) != 1) {
+      show_usage();
+      exit(1);
+    }
+  } else {
+    show_usage();
+    exit(1);
+  }
+
+  if(count < 1 || sides < 1) {
+    show_usage();
+    exit(1);
+  }
+
+  printf("%d\n", roll_dice(count, sides));
+  exit(0);
+}
