e2e7194b75832ff8b6c244ae65759f66bcd64df4
RootCellar <RootCellar9877@gmail.com>
2024-09-14 18:45:41 -0700

    add additional display option; improve random number seeding; improve show_usage()

--- dice.c
+++ dice.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include <time.h>
 
+#include "time.h"
 #include "random.h"
 
 void show_usage(void) {
-  printf("Usage: dice [count] <sides>\n");
+  printf("Usage: dice [[s[s]]count] <sides>\n");
   exit(1);
 }
 
 int main(int argc, char** argv) {
 
-  srand(time(NULL));
+  srand(get_time().tv_nsec);
 
   int sides;
   int count = 1;
 
   int separate = 0;
+  int show_sum = 0;
 
   if(argc < 2) {
     show_usage();
@@ -24,11 +26,14 @@ int main(int argc, char** argv) {
       show_usage();
     }
   } else if(argc == 3) {
-    if(sscanf(argv[1], "%d", &count) != 1) {
-      separate = 1;
-      if(sscanf(argv[1], "s%d", &count) != 1) {
+    if(sscanf(argv[1], "%d", &count) == 1) {
+
+    } else if(sscanf(argv[1], "s%d", &count) == 1) {
+        separate = 1;
+    } else if(sscanf(argv[1], "ss%d", &count) == 1) {
+        show_sum = separate = 1;
+    } else {
         show_usage();
-      }
     }
 
     if(sscanf(argv[2], "%d", &sides) != 1) {
@@ -44,10 +49,14 @@ int main(int argc, char** argv) {
 
   if(!separate) printf("%d\n", roll_dice(count, sides));
   else {
+    int sum = 0;
     for(int i = 0; i < count; i++) {
-      printf("%d ", roll_die(sides));
+      int roll = roll_die(sides);
+      sum += roll;
+      printf("%d ", roll);
     }
     printf("\n");
+    if(show_sum) printf("%d\n", sum);
   }
   exit(0);
 }
