6a1e02d88d6fb9cf4f6373bb8f63151e82687d14
RootCellar <RootCellar9877@gmail.com>
2025-04-18 22:26:23 -0800

    split time.h into header and source files

--- time.h
+++ time.h
@@ -5,27 +5,8 @@
 #include <time.h>
 #include <sys/time.h>
 
-double timespec_to_double(struct timespec time) {
-  double nanoseconds_in_seconds = (double) time.tv_nsec / 1000000000.0;
-  double seconds = time.tv_sec;
-  double timespec_in_seconds = seconds + nanoseconds_in_seconds;
-  return timespec_in_seconds;
-}
-
-double timespec_difference_seconds(struct timespec start, struct timespec end) {
-  // Shrink tv_sec down to the actual integer difference between them
-  // Helps protect time measurements from floating point roundoff because
-  // tv_sec is a large number
-  end.tv_sec -= start.tv_sec;
-  start.tv_sec = 0;
-
-  return timespec_to_double(end) - timespec_to_double(start);
-}
-
-struct timespec get_time() {
-  struct timespec time;
-  clock_gettime(CLOCK_REALTIME, &time);
-  return time;
-}
+double timespec_to_double(struct timespec time);
+double timespec_difference_seconds(struct timespec start, struct timespec end);
+struct timespec get_time();
 
 #endif //TIME_H
