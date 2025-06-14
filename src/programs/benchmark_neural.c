c1872519789833a6281a9d3993e102c65b2b8581
RootCellar <RootCellar9877@gmail.com>
2025-06-13 16:43:47 -0800

    print neural net specifications during benchmark run

--- src/programs/benchmark_neural.c
+++ src/programs/benchmark_neural.c
@@ -17,7 +17,10 @@ int main() {
 
   benchmark_setupLocale();
 
-  struct NeuralNet neural_net = create_neural_net(3, 10);
+  int layers = 3;
+  int neurons_per_layer = 10;
+
+  struct NeuralNet neural_net = create_neural_net(layers, neurons_per_layer);
 
   float inputs[] = {1.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.15, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 0.85, 0.95};
   float outputs[] = {1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0};
@@ -48,6 +51,8 @@ int main() {
     items[i].input_count = 1;
     items[i].correct_output = outputs[i];
   }
+
+  printf("Using neural net with %d layers and %d neurons per layer \n\n", layers, neurons_per_layer);
 
   TIMES( BENCHMARK_LOOPS(neural_evaluate(&neural_net, inputs, num_inputs), "neural_evaluate", BENCHMARK_ITERATIONS), BENCHMARK_TIMES);
   TIMES( BENCHMARK_LOOPS(neural_overall_error(&neural_net, items, num_inputs), "neural_overall_error", BENCHMARK_ITERATIONS), BENCHMARK_TIMES);
