8dbfd18d6631f89b25a773145f0854aa5b7fad5a
RootCellar <RootCellar9877@gmail.com>
2025-07-04 20:50:32 -0800

    prevent all created threads from running their given functions if there is an error creating any of the threads

--- src/lib/sthread.c
+++ src/lib/sthread.c
@@ -9,17 +9,30 @@ struct thread_args {
   size_t thread_count;
   void* items;
   void (*function)(size_t, size_t, size_t, void*);
+
+  pthread_mutex_t mutex;
+  int goAhead;
 };
 
 void* start_thread(void* args) {
   struct thread_args* thread_data = (struct thread_args*) (args);
-  (thread_data->function)(thread_data->id, thread_data->thread_count, thread_data->count, thread_data->items);
+
+  pthread_mutex_lock(&thread_data->mutex);
+
+  if(thread_data->goAhead) {
+    (thread_data->function)(thread_data->id, thread_data->thread_count, thread_data->count, thread_data->items);
+  }
+
+  pthread_mutex_unlock(&thread_data->mutex);
+
   pthread_exit(0);
 }
 
 void run_in_threads(void (*function)(size_t, size_t, size_t, void*), void* items, size_t count, size_t thread_count) {
   pthread_t threads[thread_count];
   struct thread_args args[thread_count];
+
+  int pthread_create_error = 0;
 
   for(size_t i = 0; i < thread_count; i++) {
     args[i].id = i;
@@ -28,13 +41,42 @@ void run_in_threads(void (*function)(size_t, size_t, size_t, void*), void* items
     args[i].count = count;
     args[i].function = function;
 
-    pthread_create(&threads[i], NULL, start_thread, (void*) &(args[i]));
+    // Lock the thread argument mutex
+    pthread_mutex_init(&args[i].mutex, NULL);
+    pthread_mutex_lock(&args[i].mutex);
 
-    // TODO: pthread error handling?
+    // Assume there will be an error, tell threads not to actually run
+    args[i].goAhead = 0;
+
+    // Create a thread, which also starts it.
+    // If there is an error making the thread, acknowledge it
+    int result = pthread_create(&threads[i], NULL, start_thread, (void*) &(args[i]));
+    if(result != 0) {
+      pthread_create_error = 1;
+    }
   }
 
+  // If there weren't any errors while making threads,
+  // tell them it's OK to run
+  if(!pthread_create_error) {
+    for(size_t i = 0; i < thread_count; i++) {
+      args[i].goAhead = 1;
+    }
+  }
+
+  // Uncnoditionally unlock the threads' mutexes
+  for(size_t i = 0; i < thread_count; i++) {
+    pthread_mutex_unlock(&args[i].mutex);
+  }
+
+  // Wait until all of the threads finish
   for(size_t i = 0; i < thread_count; i++) {
     pthread_join(threads[i], NULL);
+  }
+
+  // Destroy the mutexes
+  for(size_t i = 0; i < thread_count; i++) {
+    pthread_mutex_destroy(&args[i].mutex);
   }
 }
 
