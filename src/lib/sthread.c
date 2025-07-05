e730ddd3b1cfd375780587d1cbac25c93039a86a
RootCellar <RootCellar9877@gmail.com>
2025-07-04 16:54:41 -0800

    add startnig asynchronous thread code

--- src/lib/sthread.c
+++ src/lib/sthread.c
@@ -1,4 +1,7 @@
 #include <pthread.h>
+#include <stdlib.h>
+
+#include "sthread.h"
 
 struct thread_args {
   size_t id;
@@ -31,4 +34,32 @@ void run_in_threads(void (*function)(size_t, size_t, size_t, void*), void* items
   for(size_t i = 0; i < thread_count; i++) {
     pthread_join(threads[i], NULL);
   }
+}
+
+static
+void* execute_async(void* input) {
+  struct async_thread_args* data = (struct async_thread_args*) input;
+
+  (data->function)(data->input);
+
+  pthread_exit(0);
+}
+
+struct async_exec_data run_asynchronous(void (*function)(void*), void* input) {
+  struct async_exec_data toRet;
+
+  toRet.thread_args = malloc(sizeof(struct async_thread_args));
+
+  // TODO: malloc error handling
+  
+  toRet.thread_args->function = function;
+  toRet.thread_args->input = input;
+
+  pthread_create(&toRet.thread, NULL, execute_async, (void*) toRet.thread_args);
+
+  return toRet;
+}
+
+void await_async_function(struct async_exec_data data) {
+  pthread_join(data.thread, NULL);
 }
--- src/lib/sthread.h
+++ src/lib/sthread.h
@@ -1,7 +1,22 @@
 #ifndef STHREAD_H
 #define STHREAD_H
 
+#include <pthread.h>
+
+struct async_thread_args {
+    void* input;
+    void (*function)(void*);
+};
+
+struct async_exec_data {
+    pthread_t thread;
+    struct async_thread_args* thread_args;
+};
+
 void* start_thread(void* args);
 void run_in_threads(void (*function)(size_t, size_t, size_t, void*), void* items, size_t count, size_t thread_count);
+
+struct async_exec_data run_asynchronous(void (*)(void*), void*);
+void await_async_function(struct async_exec_data);
 
 #endif //STHREAD_H
