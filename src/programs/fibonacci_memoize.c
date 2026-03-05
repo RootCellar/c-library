e011b7ae7631911bd88d5e80dc0814163e2eeb1a
RootCellar <RootCellar9877@gmail.com>
2026-03-05 07:55:19 -0900

    shift fibonacci indexes down by 1

--- src/programs/fibonacci_memoize.c
+++ src/programs/fibonacci_memoize.c
@@ -20,7 +20,7 @@
 //
 // Fibonacci Computing Functions
 //
-// Note: These all assume that the index is 1 or greater, and isn't big enough
+// Note: These all assume that the index is 0 or greater, and isn't big enough
 // to result in an answer that overflows an unsigned, 64-bit integer.
 //
 
@@ -29,19 +29,19 @@ typedef uint64_t ANSWER_TYPE;
 
 
 ANSWER_TYPE fibonacci_recursive(const size_t index) {
-    if(index == 1 || index == 2) return 1;
+    if(index == 0 || index == 1) return 1;
 
     return fibonacci_recursive(index - 1) + fibonacci_recursive(index - 2);
 }
 
 ANSWER_TYPE fibonacci_loop(const size_t index) {
-    if(index == 1 || index == 2) return 1;
+    if(index == 0 || index == 1) return 1;
 
     ANSWER_TYPE a = 1;
     ANSWER_TYPE b = 1;
     ANSWER_TYPE answer = 0;
 
-    for(size_t i = 3; i <= index; i++) {
+    for(size_t i = 2; i <= index; i++) {
         answer = a + b;
         a = b;
         b = answer;
@@ -51,12 +51,12 @@ ANSWER_TYPE fibonacci_loop(const size_t index) {
 }
 
 ANSWER_TYPE fibonacci_memoized(const size_t index, ANSWER_TYPE* memory, const size_t memory_length) {
-    const size_t memory_index = index - 1;
+    const size_t memory_index = index;
     if(index <= memory_length && memory[memory_index] > 0) return memory[memory_index];
 
     ANSWER_TYPE answer;
 
-    if(index == 1 || index == 2) {
+    if(index == 0 || index == 1) {
         answer = 1;
     }
     else {
@@ -81,7 +81,7 @@ int main(int argc, char** argv) {
         EXIT_FAIL();
     }
 
-    for(size_t i = 1; i <= 10; i++) {
+    for(size_t i = 0; i <= 9; i++) {
         printf("fibonacci_recursive(%zu) = %llu\n", i, fibonacci_recursive(i));
         printf("fibonacci_loop(%zu) = %llu\n", i, fibonacci_loop(i));
         printf("fibonacci_memoize(%zu) = %llu\n", i, fibonacci_memoized(i, memoize_memory, memoize_memory_length));
