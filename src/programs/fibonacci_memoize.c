68ca1868bf2e68f8223195ede2efe0445a5c716f
RootCellar <RootCellar9877@gmail.com>
2026-03-05 07:40:36 -0900

    begin fibonacci_memoize

---
+++ src/programs/fibonacci_memoize.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include "../lib/compiler_attributes.h"
+#include "../lib/debug.h"
+#include "../lib/unit_testing.h"
+#include "../lib/benchmark.h"
+
+int main(int argc, char** argv) {
+    exit(0);
+}
